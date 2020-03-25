80a477dcb7bf16a12726aa39749e5e7d33e4a22b
nbelisle <nicolebelisle121@gmail.com>
2020-03-25 16:09:03 -0400

    Calculated whether patients blood types were a match

--- blood_match.py
+++ blood_match.py
@@ -12,5 +12,18 @@ b1 = requests.get("http://vcm-7631.vm.duke.edu:5002/get_blood_type/F6")
 b2 = requests.get("http://vcm-7631.vm.duke.edu:5002/get_blood_type/M8")
 print(b1.text)
 print(b2.text)
+bt1 =b1.text
+bt2 =b2.text
 
-#
+# Calculate if blood types from recipient and donor are a match
+
+if bt1 == bt2: 
+    print("Patient F6 and M8 are a match")
+else:
+    print ("Patient F6 and M8 are not a match")
+
+# Check result with a POST request
+# Check your result by making a POST request to URL/match_check. Send a JSON with the following format:
+# {"Name": "<name>", "Match": "<answer>"}
+# Replace <name> with your Duke Net ID and <answer> with either Yes or No.
+# This request will return "Correct" or "Incorrect".
